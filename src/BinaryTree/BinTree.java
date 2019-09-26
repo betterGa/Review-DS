332b33aa2da093f0424b88f7567f33b6ac2889fc
yourjia <2086543608@qq.com>
2019-09-26 20:57:21 +0800

    二叉树

--- src/BinaryTree/BinTree.java
+++ src/BinaryTree/BinTree.java
@@ -1,4 +1,19 @@
 package BinaryTree;
 
-public interface BinTree {
+public interface BinTree<E>{
+void add(E e);
+boolean contains(E e);
+E getNin();
+E getMax();
+E removeMax();
+E removeMin();
+boolean remove(E e);
+int getSize();
+//遍历
+    //前序
+    void preOrder();
+    //中序
+    void inOrder();
+//后序
+    void postOrder();
 }
