c6423fef7f755fec5102142b7d6148c2130ebd11
yourjia <2086543608@qq.com>
2019-12-16 20:05:17 +0800

    20191216

--- src/SubClass.java
+++ src/SubClass.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
-
-public class Super {
+/*
+public class SubClass
+{
+    public static void main(String[] args) {
+     final StringBuilder s=new StringBuilder("Hello");
+     s.append("world");
+     System.out.println(s);
+    }
 }
+*/
+
+public class SubClass
+{
+    public static int testFinally1()
+    {
+        int result=1;
+        try
+        {
+            result=2;
+            return result;
+        }
+        finally {
+            result=3;
+            System.out.println("execute finally1");
+        }
+    }
+    public static StringBuffer testfinally2()
+    {StringBuffer s=new StringBuffer("Hello");
+    try
+    {
+        return s;
+    }
+
+    catch (Exception e)
+    {return null;}
+    finally {
+        s.append("World");
+        System.out.println("execute finally2");
+    }
+    }
+
+
+
+    public static void main(String[] args) {
+        int resultVal = testFinally1();
+        System.out.println(resultVal);
+        StringBuffer resultRef = testfinally2();
+        System.out.println(resultRef);
+    }}
