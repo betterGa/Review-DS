d968634d3b0a49cc7cdc8bde4a3d8a144873bb5d
yourjia <2086543608@qq.com>
2019-12-17 17:45:55 +0800

    20191217

--- src/SubClass.java
+++ src/SubClass.java
@@ -1,49 +1,18 @@
-/*
 public class SubClass
 {
-    public static void main(String[] args) {
-     final StringBuilder s=new StringBuilder("Hello");
-     s.append("world");
-     System.out.println(s);
+    public static void test(StringBuffer ss1,int n)
+    {
+        ss1.append("World");
+        n=8;
     }
+
+    public static void main(String[] args) {
+      int i=1;
+        StringBuffer s1=new StringBuffer("Hello");
+        test(s1,i);
+        System.out.println(s1+"i="+i);
+    }
+
+
+
 }
-*/
-
-public class SubClass
-{
-    public static int testFinally1()
-    {
-        int result=1;
-        try
-        {
-            result=2;
-            return result;
-        }
-        finally {
-            result=3;
-            System.out.println("execute finally1");
-        }
-    }
-    public static StringBuffer testfinally2()
-    {StringBuffer s=new StringBuffer("Hello");
-    try
-    {
-        return s;
-    }
-
-    catch (Exception e)
-    {return null;}
-    finally {
-        s.append("World");
-        System.out.println("execute finally2");
-    }
-    }
-
-
-
-    public static void main(String[] args) {
-        int resultVal = testFinally1();
-        System.out.println(resultVal);
-        StringBuffer resultRef = testfinally2();
-        System.out.println(resultRef);
-    }}
